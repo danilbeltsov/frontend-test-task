75aa8dde48b1f74e6d8cd5812e9ada90571d0430
Danil Beltsov <danilbeltsov@gmail.com>
2024-10-17 15:16:37 +0300

    feat(carousel): remove unused code

--- src/app/core/api/banner-service.service.ts
+++
@@ -1,9 +0,0 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class BannerServiceService {
-  private httpClient: HttpClient = inject(HttpClient);
-}
